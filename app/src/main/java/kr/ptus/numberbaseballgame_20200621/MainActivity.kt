43076349b8c1281e1c12101f5895467fcf685b18
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-06-21 14:15:13 +0900

    내가 말한 내용 (버튼 누르면) 실제 반영

--- app/src/main/java/kr/ptus/numberbaseballgame_20200621/MainActivity.kt
+++ app/src/main/java/kr/ptus/numberbaseballgame_20200621/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.ptus.numberbaseballgame_20200621
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kr.ptus.numberbaseballgame_20200621.adapters.ChatAdapter
 import kr.ptus.numberbaseballgame_20200621.datas.Chat
@@ -9,7 +10,7 @@ import kr.ptus.numberbaseballgame_20200621.datas.Chat
 class MainActivity : BaseActivity() {
 
     val chatList = ArrayList<Chat>()
-    lateinit var mChatAdapter : ChatAdapter
+    lateinit var mChatAdapter: ChatAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,17 +21,36 @@ class MainActivity : BaseActivity() {
         setValues()
 
     }
+
     override fun setupEvents() {
 
+        inputBtn.setOnClickListener {
+
+            val inputNum = numberEdt.text.toString()
+
+            if (inputNum.length != 3) {
+
+                Toast.makeText(mContext, "세자리 숫자만 입력 가능합니다.", Toast.LENGTH_SHORT).show()
+
+                return@setOnClickListener
+            }
+
+            chatList.add(Chat("ME" , inputNum))
+
+            mChatAdapter.notifyDataSetChanged()
+
+
+
+        }
 
 
     }
 
     override fun setValues() {
 
-        chatList.add(Chat("CPU","숫자야구 게임에 오신 것을 환영합니다."))
-        chatList.add(Chat("CPU","세자리 숫자를 맞춰주세요."))
-        chatList.add(Chat("CPU","0은 포함되지 않으며, 중복된 숫자도 없습니다."))
+        chatList.add(Chat("CPU", "숫자야구 게임에 오신 것을 환영합니다."))
+        chatList.add(Chat("CPU", "세자리 숫자를 맞춰주세요."))
+        chatList.add(Chat("CPU", "0은 포함되지 않으며, 중복된 숫자도 없습니다."))
 
         mChatAdapter = ChatAdapter(mContext, R.layout.chat_list_item, chatList)
         ChatListView.adapter = mChatAdapter
