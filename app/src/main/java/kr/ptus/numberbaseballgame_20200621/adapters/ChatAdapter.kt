fbb5b0efc8dac15468166b30c7ab102ddbd31bdb
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-06-21 13:57:08 +0900

    채팅 관련 Adapter 뼈대 작업

---
+++ app/src/main/java/kr/ptus/numberbaseballgame_20200621/adapters/ChatAdapter.kt
@@ -0,0 +1,42 @@
+package kr.ptus.numberbaseballgame_20200621.adapters
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.TextView
+import kr.ptus.numberbaseballgame_20200621.R
+import kr.ptus.numberbaseballgame_20200621.datas.Chat
+import org.json.JSONObject
+import org.w3c.dom.Text
+
+class ChatAdapter(val mContext: Context, val resId: Int, val mList: List<Chat>) :
+    ArrayAdapter<Chat>(mContext, resId, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    @SuppressLint("ResourceAsColor")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+
+        tempRow?.let {
+
+        }.let {
+            tempRow = inf.inflate(R.layout.chat_list_item, null)
+        }
+
+        val row = tempRow!!
+
+
+        return row
+
+
+    }
+
+}
